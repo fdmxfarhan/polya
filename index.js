51b2637b7eba2e9bfcb1986a5d9c0daa864db9c0
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-09-07 18:55:25 +0430

    asdf

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ const io = require('socket.io')(httpsServer);
 
 
 httpServer.listen(3000);
-httpsServer.listen(443);
+httpsServer.listen(4443);
 console.log('server is started :)')
 
 // app.listen(port, () => {
