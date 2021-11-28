ae405de066a8dbcc065ddb5a4825b30be91c2f98
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-11-28 13:11:25 +0330

    asdf

--- routes/payment.js
+++ routes/payment.js
@@ -44,8 +44,8 @@ router.get('/pay', ensureAuthenticated, function(req, res, next) {
             url: 'https://api.idpay.ir/v1.1/payment',
             headers: {
                 'Content-Type': 'application/json',
-                'X-API-KEY': 'dec2b2aa-2cb5-47f4-8584-963dc313f363',
-                'X-SANDBOX': 1,
+                'X-API-KEY': 'edda26ba-74aa-4333-9bee-f51dbe7475b6',
+                // 'X-SANDBOX': 1,
             },
             body: {
                 'order_id': payment._id,
@@ -53,7 +53,7 @@ router.get('/pay', ensureAuthenticated, function(req, res, next) {
                 'name': payment.fullname,
                 'email': payment.email,
                 'desc': payment.description,
-                'callback': 'http://185.141.107.167/payment/pay',
+                'callback': 'https://cartan.ir/payment/pay',
                 'reseller': null,
             },
             json: true,
@@ -75,9 +75,8 @@ router.post('/pay', function(req, res, next) {
                 url: 'https://api.idpay.ir/v1.1/payment/verify',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-API-KEY': 'dec2b2aa-2cb5-47f4-8584-963dc313f363',
-                    // 'X-API-KEY': 'fe6a4553-cd95-4dff-af2e-80594c1c18c5',
-                    'X-SANDBOX': 1,
+                    'X-API-KEY': 'edda26ba-74aa-4333-9bee-f51dbe7475b6',
+                    // 'X-SANDBOX': 1,
                 },
                 body: {
                     'id': req.body.id,
